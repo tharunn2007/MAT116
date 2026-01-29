9aa1b7763934758ada8aa93647e4f91745634d32
Tharunn D <83963600+tharunn2007@users.noreply.github.com>
2026-01-29 11:35:44 +0530

    Add implication and biconditional logic functions

--- 28_1_26_MAT116LAB1.py
+++ 28_1_26_MAT116LAB1.py
@@ -20,4 +20,27 @@ for p in (True,False):
         print (p,"\t",q ,"\t",exclusive_or(p,q))
 print()
 
+#implication logic 
+def implication(p,q):
+    return not p or q
+print("____________")
+print("p \t q \t p IMPLIES q")
+print("____________")
+for p in (True,False):
+    for q in (True,False):
+        print (p,"\t",q ,"\t",implication(p,q))
+print()
+
+#biconditional logic
+def biconditional(p,q):
+    return not((p and not q) or (q and not p))
+print("____________")
+print("p \t q \t p only if q")
+print("____________")
+for p in (True,False):
+    for q in (True,False):
+        print (p,"\t",q ,"\t",biconditional(p,q))
+print()
+
+
 
